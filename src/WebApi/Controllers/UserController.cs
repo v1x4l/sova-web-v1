7fd8074ad09c1b7dc80dde6fbd32a5896e22923c
Lasse <lassegp@hotmail.dk>
2016-11-23 14:22:02 +0100

    Nyeste, rettet fejl med users

--- src/WebApi/Controllers/UserController.cs
+++ src/WebApi/Controllers/UserController.cs
@@ -16,7 +16,7 @@ namespace WebApi.Controllers
         {
         }
 
-        // GET api/values
+        
         [HttpGet(Name = Config.UsersRoute)]
         public IActionResult Get(int page = 0, int pagesize = Config.DefaultPageSize)
         {
@@ -38,7 +38,7 @@ namespace WebApi.Controllers
 
 
 
-        // GET api/values/5
+        
         [HttpGet("{id}", Name = Config.UserRoute)]
         public IActionResult Get(int id)
         {
@@ -47,7 +47,7 @@ namespace WebApi.Controllers
             return Ok(ModelFactory.Map(user, Url));
         }
 
-        // POST api/values
+        
         [HttpPost]
         public IActionResult Post([FromBody] UserModel model)
         {
@@ -56,7 +56,7 @@ namespace WebApi.Controllers
             return Ok(ModelFactory.Map(user, Url));
         }
 
-        // PUT api/values/5
+        
         [HttpPut("{id}")]
         public IActionResult Put(int id, [FromBody] UserModel model)
         {
@@ -69,7 +69,7 @@ namespace WebApi.Controllers
             return Ok();
         }
 
-        // DELETE api/values/5
+        
         [HttpDelete("{id}")]
         public IActionResult Delete(int id)
         {
